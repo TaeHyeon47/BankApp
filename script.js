751a8b54c8e2f6cf8716000c29d6f3b954a0666d
Tae Hyeon Jo <hillskuti@naver.com>
2023-01-10 21:32:29 +0900

    reduce 연습

--- script.js
+++ script.js
@@ -6,31 +6,31 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -62,61 +62,59 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 const dispalyMovements = function (movements) {
-  containerMovements.innerHTML = '';
+    containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
-
-    const html = `
+    movements.forEach(function (mov, i) {
+        const type = mov > 0 ? 'deposit' : 'withdrawal';
+        const html = `
     <div class="movements__row">
       <div class="movements__type movements__type--${type}">${
-      i + 1
-    } ${type}</div>
+            i + 1
+        } ${type}</div>
       <div class="movements__value">${mov}</div>
     </div>
     `;
-
-    containerMovements.insertAdjacentHTML('afterbegin', html);
-  });
+        containerMovements.insertAdjacentHTML('afterbegin', html);
+    });
 };
 
 dispalyMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
-};
-
+    const balance = movements.reduce((acc, mov) => acc + mov, 0);
+    labelBalance.textContent = `${balance} EUR`;
+}
 calcDisplayBalance(account1.movements);
 
 const createUsernames = function (accs) {
-  accs.forEach(function (acc) {
-    acc.username = acc.owner
-      .toLowerCase()
-      .split(' ')
-      .map((name) => name[0])
-      .join('');
-  });
+    accs.forEach(function (acc) {
+        acc.username = acc.owner
+            .toLowerCase()
+            .split(' ')
+            .map((name) => name[0])
+            .join('');
+    });
 };
 
 createUsernames(accounts);
-console.log(accounts);
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
 const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
 console.log(
-  '%c%s',
-  'background-color: #e0005a ; color: #ffffff ; font-weight: bold ; padding: 4px ;',
-  'script.js - (98) ~ dispalyMovements ~ movements: ' + movements
+    '%c%s',
+    'background-color: #e0005a ; color: #ffffff ; font-weight: bold ; padding: 4px ;',
+    'script.js - (98) ~ dispalyMovements ~ movements: ' + movements
 );
 
 /////////////////////////////////////////////////
@@ -189,18 +187,18 @@ console.log(movements);
 console.log(movementsUSDfor);
 
 const movementsDescriptions = movements.map(
-  (mov, i, arr) =>
-    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs()}`
-  // if (mov > 0) {
-  //   return `Movement ${i + 1}: You deposited ${mov}`;
-  // } else {
-  //   return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
-  // }
+    (mov, i, arr) =>
+        `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs()}`
+    // if (mov > 0) {
+    //   return `Movement ${i + 1}: You deposited ${mov}`;
+    // } else {
+    //   return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
+    // }
 );
 console.table(movementsDescriptions);
 
 const deposits = movements.filter(function (mov) {
-  return mov > 0;
+    return mov > 0;
 });
 
 console.log(movements);
@@ -229,8 +227,11 @@ console.log(balance2);
 
 // Maximum value
 const max = movements.reduce((acc, mov) => {
-  if (acc > mov) return acc;
-  else return mov;
+    if (acc > mov) return acc;
+    else return mov;
 }, movements[0]);
 
 console.log(max);
+
+
+
